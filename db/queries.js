47eca8a0fbbaa3d144de193d6b2f052cdb30bf52
Samuel <samuferfort@gmail.com>
2024-08-05 14:50:33 +0200

    create queries for user model

--- db/queries.js
+++ db/queries.js
@@ -1,20 +1,29 @@
-const pool = require("./db/pool")
-
+const pool = require("./db/pool");
 
 module.exports = {
-    getUserByUsername: async (username) => {
-        const result = await pool.query("SELECT * FROM users WHERE username = $1", [username])
-        return result.rows[0]
-    },
+  getUserByUsername: async (username) => {
+    const result = await pool.query("SELECT * FROM users WHERE username = $1", [
+      username,
+    ]);
+    return result.rows[0];
+  },
 
-    createUser: async(user) => {
-        const {username, password, first_name, last_name, membership_status} = user
-        const result = await pool.query(
-            "INSERT INTO users (username, password, first_name, last_name, membership_status) VALUES ($1, $2, $3, $4, $5) RETURNING *",
-            [username, password, first_name, last_name, membership_status]
-        )
-        return result.rows[0]
-    }
-    
+  createUser: async (user) => {
+    const { username, password, first_name, last_name, membership_status } =
+      user;
+    const result = await pool.query(
+      "INSERT INTO users (username, password, first_name, last_name, membership_status) VALUES ($1, $2, $3, $4, $5) RETURNING *",
+      [username, password, first_name, last_name, membership_status]
+    );
+    return result.rows[0];
+  },
 
-}
+  findByIdAndUpdateMembership: async (id) => {
+    const result = await pool.query(
+      "UPDATE users SET membership_status = $1 WHERE id = $2 RETURNING *",
+      ["member", id]
+    );
+
+    return result.rows[0];
+  },
+};
